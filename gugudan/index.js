276195a62525e4aa80e391239ec0001d56e0d5f4
sarangbokki <91423376+sarangbokki@users.noreply.github.com>
2021-09-28 23:32:42 +0900

    (마침.) 구구단

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,18 +1,26 @@
-/*버튼을 클릭할 때~ */
+    /*const로 변수에 값을 저장하기 = 값 가져오기(대입)*/
+
+
+
+    /*버튼 가져오기(샵 클릭까지) // 버튼을 클릭하기.addEventListener부터 끝까지*/
 document.querySelector('#click').addEventListener('click', () => {
-    /*값 불러오기 + const로 변수에 값을 저장하기*/
     const a = document.querySelector('#first').value
     const b = document.querySelector('#second').value
+
     if (a) {
         if (b) {
-            a * b /*밑에 줄은 곱한 값을 표시 그래서 result */
-            document.querySelector('#result').textContent = a * b
+            /* 꼭 저장을 위에 할 필요 없음! 바로 위에 해도 OK!!*/
+            const c = a * b
+            const r = document.querySelector('#result')
+            r.textContent = c
         } else {
-            document.querySelector('#result').textContent = '두 번째 값 입력해'
+            const r = document.querySelector('#result')
+            r.textContent = '두 번째 값 입력해'
         }
 
     } else {
-        document.querySelector('#result').textContent = '첫 번째 값 입력해'
+        const r = document.querySelector('#result')
+        r.textContent = '첫 번째 값 입력해'
     }
 });
 
