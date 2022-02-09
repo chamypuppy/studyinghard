e0fa2048b7773a3317fc31461a45e9b0bca63b83
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-09 15:03:17 +0900

    디저트로 복습하는 array

--- dessertArray.js
+++ dessertArray.js
@@ -1,7 +1,11 @@
 const dessert = ['cake', 1, 'pudding', 'applePie', 'cookies'];
 dessert.push('homeRunBall');
 console.log(dessert);
-console.log(dessert[5]); 
+console.log(dessert[5]);
+//↓↓ 안의 index 항목을 변경할 수도 있다!!! ↓↓↓
+dessert[1] = "hobbang";
+
+console.log(dessert);
 
 const me = "sexy";
 const days = [1, 2, true, false, null, undefined, me];
