453ab87724a5f79ce90360beef6058bdcc84a173
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-09 14:58:54 +0900

    디저트로 복습하는 array

--- dessertArray.js
+++ dessertArray.js
@@ -2,3 +2,7 @@ const dessert = ['cake', 1, 'pudding', 'applePie', 'cookies'];
 dessert.push('homeRunBall');
 console.log(dessert);
 console.log(dessert[5]); 
+
+const me = "sexy";
+const days = [1, 2, true, false, null, undefined, me];
+console.log(days);
