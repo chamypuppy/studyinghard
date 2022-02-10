e77ca4c5e54acb08ca5650211f136bf54f31893f
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-10 14:14:09 +0900

    function의 기초

---
+++ function-foundation.js
@@ -0,0 +1,17 @@
+function sayHello() {
+  console.log('Hello!');
+}
+
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+
+function food() {
+  console.log("i love food!!!");
+}
+
+food()
+food()
+food()
+food()
