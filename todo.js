b1f3d7cd3123a71d6f2bd58841ae76d1ac117552
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-23 16:08:12 +0900

    크롬 to do list 만들기
    여태까지 배운 이론들을 활용하여 to do list 코드 만들기 (노마드코더 바닐라 JS 크롬 클론 코딩)

---
+++ todo.js
@@ -0,0 +1,23 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo){
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span); /* li 안에 span 태그 삽입 */
+  span.innerText = newTodo;
+  console.log(li);
+  toDoList.appendChild(li);
+}
+
+
+function handleToDoSubmit(event){
+  event.preventDefault();
+  const newTodo = toDoInput.value; //input의 입력한 value를 새로운 newTodo라는 변수에 복사(저장)
+  toDoInput.value = ""; /* 그 다음에 우리가 이렇게 값을 empty해도 newTodo변수와는 관련이 없다 */
+  paintToDo(newTodo);
+}
+
+
+toDoForm.addEventListener("submit", handleToDoSubmit)
