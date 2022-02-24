91500aff8a0cebc9d841025999571eeb4a140748
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-24 15:59:22 +0900

    입력한 todolist와 새로운 todolist 화면 상에 잘 노출 시키기
    새로 입력한 todolist와 기존에 등록해둔 todolist를 화면에 잘 띄우기.
    이때 localStorage 또한 기존 + 새로 입력한 todolist에 대한 array가 replace되지 않고 둘 다(기존+새로 입력 todolist) 값을 가질 수 있다!
    파일 이름을 todo-.js로 하여 따로 업로드하는 이유는 그 전 단계의 로직도 파악하면서, 새롭게 업로드 되어 필요 없어져서 지워진 코드들도 같이 보존하고 싶어서 이다 ㅠ.ㅠ
    (완성된 파일보다 미완성된 파일에서 로직을 더 잘 이해할 수 있는 것 같다..)
    todo.js -> todo-.js (최신 업데이트 코드) 이다.

---
+++ todo-.js
@@ -0,0 +1,52 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos(){ /* 이 함수가 하는 단 한 가지 : toDos array의 내용을 localStorage에 넣는 것 */
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo){
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo)
+  li.appendChild(span); /* li 안에 span 태그 삽입 */
+  li.appendChild(button); 
+  toDoList.appendChild(li);
+}
+
+
+function handleToDoSubmit(event){
+  event.preventDefault();
+  const newTodo = toDoInput.value; //input의 입력한 value를 새로운 newTodo라는 변수에 복사(저장)
+  toDoInput.value = ""; /* 그 다음에 우리가 이렇게 값을 empty해도 newTodo변수와는 관련이 없다 */
+  toDos.push(newTodo); /* user가 입력한 todolist인 newTodo를 빈 array인 toDos에 삽입 */
+  paintToDo(newTodo); /* 화면에 그 ToDo를 보여준다. */
+  saveToDos();
+}
+
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY); /* localStorage에 todos 카테고리의 값을 가져온다 */
+// console.log(savedToDos) /* 그냥 단순한 string */
+
+if(savedToDos !== null){ /* 만약 savedToDos가 있다면 */
+    const paresdToDos = JSON.parse(savedToDos);/* 살아있는 js object(= 살아있는 array)*/
+    toDos = paresdToDos; /* 살아있는 array를 빈 array에 넣어(대입) */
+    paresdToDos.forEach(paintToDo);
+
+}
+
