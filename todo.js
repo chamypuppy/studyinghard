9a03c1159b538f373fad34fdea7bbb4111dc9fdb
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-24 15:04:19 +0900

    todolist | string에서 살아있는 array로
    JSON.stringify() => localStroage에 저장된 array를 string으로
    JSON.parse() => stringify에서 나온 지루한 string을 살아있는 JS object이자 array로 만들어준다.
    그리고 forEach라는 함수를 통해 살아있는 array의 각각의 item에 function을 실행할 수 있게 해준다.

--- todo.js
+++ todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){ /* 이 함수가 하는 단 한 가지 : toDos array의 내용을 localStorage에 넣는 것 */
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
   const li = event.target.parentElement;
   li.remove();
@@ -24,8 +32,24 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newTodo = toDoInput.value; //input의 입력한 value를 새로운 newTodo라는 변수에 복사(저장)
   toDoInput.value = ""; /* 그 다음에 우리가 이렇게 값을 empty해도 newTodo변수와는 관련이 없다 */
-  paintToDo(newTodo);
+  toDos.push(newTodo); /* user가 입력한 todolist인 newTodo를 빈 array인 toDos에 삽입 */
+  paintToDo(newTodo); /* 화면에 그 ToDo를 보여준다. */
+  saveToDos();
 }
 
 
-toDoForm.addEventListener("submit", handleToDoSubmit)
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item){
+  console.log("this is the turn of", item);
+}
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY); /* localStorage에 todos 카테고리의 값을 가져온다 */
+// console.log(savedToDos) /* 그냥 단순한 string */
+
+if(savedToDos !== null){ /* 만약 savedToDos가 있다면 */
+    const paresdToDos = JSON.parse(savedToDos)/* 그 가져온 값(savedToDos)이 살아있는 js object(또 다른 말로는 살아있는 array)로 변할거야 */
+    // console.log(paresdToDos); /* 살아있는 js object이자 array */
+    paresdToDos.forEach(sayHello); /* forEach는 각각의 item에 대해 sayHello를 실행시켜 */
+}
