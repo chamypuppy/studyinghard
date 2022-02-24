99aa81ebc7191aae62d2c15d2679606d3e4e1817
세숑 <91423376+chamypuppy@users.noreply.github.com>
2022-02-24 12:38:28 +0900

    todolist에 ❌버튼 만들고 ❌버튼으로  todo항목들 제거하기

--- todo.js
+++ todo.js
@@ -2,12 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo){
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span); /* li 안에 span 태그 삽입 */
   span.innerText = newTodo;
-  console.log(li);
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo)
+  li.appendChild(span); /* li 안에 span 태그 삽입 */
+  li.appendChild(button); 
   toDoList.appendChild(li);
 }
 
